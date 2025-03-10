c854215477af36d0a80a523ceb7a332e65b45428
isempa <mssempai@gmail.com>
2025-03-10 14:06:30 +0300

    Музыка

---
+++ welcom_screen.gd
@@ -0,0 +1,10 @@
+extends Control
+
+func _ready():
+	visible = true  # Окно показывается при старте
+
+func _on_продолжить_pressed():
+	print("Кнопка нажата!")  # Проверяем, работает ли нажатие
+	visible = false  # Скрываем окно
+# Воспроизводим звук (убедись, что в дереве есть узел AudioStreamPlayer)
+	$AudioStreamPlayer2D.play()
