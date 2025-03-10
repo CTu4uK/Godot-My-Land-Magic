91e99813d49c41f6d04f04b4bc44127b3e9be8a8
isempa <mssempai@gmail.com>
2025-03-10 14:20:30 +0300

    музыка меню

--- mainmenu.gd
+++ mainmenu.gd
@@ -2,3 +2,5 @@ extends VBoxContainer
 
 func _on_новая_игра_pressed():
 	get_tree().change_scene_to_file("res://node_2d.tscn")
+# Воспроизводим звук (убедись, что в дереве есть узел AudioStreamPlayer)
+	$AudioStreamPlayer2D.play()
